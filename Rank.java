215da97d5441187d6614372cf9398c17d92b948a
АЛЕКС ГЕОРГИЕВ <s23621625@onlineedu.tu-varna.bg>
2025-05-15 16:10:56 +0300

    Add files via upload

--- Rank.java
+++ Rank.java
@@ -1,18 +1,12 @@
+package models;
+
 public enum Rank {
-    YOUNGLING, INITIATE, PADAWAN, KNIGHT_ASPIRANT,
-    KNIGHT, MASTER, BATTLE_MASTER, GRAND_MASTER;
-
-    public static Rank promote(Rank rank) {
-        if (rank.ordinal() < GRAND_MASTER.ordinal()) {
-            return Rank.values()[rank.ordinal() + 1];
-        }
-        return rank;
-    }
-
-    public static Rank demote(Rank rank) {
-        if (rank.ordinal() > YOUNGLING.ordinal()) {
-            return Rank.values()[rank.ordinal() - 1];
-        }
-        return rank;
-    }
+    YOUNGLING,
+    INITIATE,
+    PADAWAN,
+    KNIGHT_ASPIRANT,
+    KNIGHT,
+    MASTER,
+    BATTLE_MASTER,
+    GRAND_MASTER
 }
